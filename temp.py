7f6f8b860a1f335dbf58448c438f63ddf9adedca
askaaska <2929aska@gmail.com>
2022-03-30 16:01:34 +0900

    1st cocmmit

---
+++ temp.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.write('テスト')
+st.write(pd.DataFrame({'1列目': [1, 2, 3, 4],'２列目': [5, 6, 7, 8]}))
+
+import streamlit as st
+import pandas as pd
+
+st.write('テスト')
+st.write(pd.DataFrame({'1列目': [1, 2, 3, 4],'２列目': [5, 6, 7, 8]}))
+
